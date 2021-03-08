315699eebcc184f948ce4e570c456656540f3f50
Kirchhoff- <kirchhoff.19.92@gmail.com>
2021-03-08 15:44:22 +0200

    Create navigation_graph.xml and LoginFragment.kt. Used navigation_graph.xml in MainActivity.kt

---
+++ presentation/src/main/java/com/kirchhoff/presentation/ui/login/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.kirchhoff.presentation.ui.login
+
+import com.kirchhoff.presentation.R
+import com.kirchhoff.presentation.ui.base.BaseFragment
+
+class LoginFragment : BaseFragment(R.layout.fragment_login)
